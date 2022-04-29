9e051205a122a9a4373b6f3f26090d5acda73152
brandonb528 <broobran528@gmail.com>
2022-04-29 00:00:59 -0400

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+//What's happening? 
+
+//Get user choice
+//get computer choice
+//compare user and computer choice
+//return results
+
+
+
+
+let choices = ['rock', 'paper', 'scissors'];
+
+function computerPlay() {
+    let randomChoice = Math.floor(Math.random() * (3 - 0) + 0);
+    return choices[randomChoice];
+}
+
+console.log(computerPlay());
