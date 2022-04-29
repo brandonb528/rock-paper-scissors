d8be4c5181b4a3d2c4f900ad48853dc084db6a9c
brandonb528 <broobran528@gmail.com>
2022-04-29 00:48:08 -0400

    Add a win check and choice comparison function

--- script.js
+++ script.js
@@ -10,14 +10,28 @@
 
 let choices = ['rock', 'paper', 'scissors'];
 
-function userChoice() {
+let userChoice = () => {
     let userChoice = parseInt(prompt('Rock Paper Scissors? (1 for rock, 2 for paper, 3 for scissors')) - 1;
     return choices[userChoice];
 }
 
-function computerPlay() {
-    let randomChoice = Math.floor(Math.random() * (3 - 0) + 0);
+let computerChoice = () => {
+    let randomChoice = 1//Math.floor(Math.random() * (3 - 0) + 0);
     return choices[randomChoice];
+};
+
+function winCheck(userChoice, computerChoice) {
+    if (userChoice === choices[0] && computerChoice === choices[2] || 
+        userChoice === choices[1] && computerChoice === choices[0] ||
+        userChoice === choices[2] && computerChoice === choices[1]) {
+        
+        console.log('you win!');
+    }
 }
 
-console.log(userChoice());
+
+function compareChoices(userChoice,computerChoice) {
+    winCheck(userChoice, computerChoice);
+}
+
+compareChoices(userChoice(), computerChoice())
