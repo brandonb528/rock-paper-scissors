8e5ae30a7175fd7ed271444cfc16e4d4286f33d6
brandonb528 <broobran528@gmail.com>
2022-04-29 00:07:52 -0400

    Add user choice function

--- script.js
+++ script.js
@@ -10,9 +10,14 @@
 
 let choices = ['rock', 'paper', 'scissors'];
 
+function userChoice() {
+    let userChoice = parseInt(prompt('Rock Paper Scissors? (1 for rock, 2 for paper, 3 for scissors')) - 1;
+    return choices[userChoice];
+}
+
 function computerPlay() {
     let randomChoice = Math.floor(Math.random() * (3 - 0) + 0);
     return choices[randomChoice];
 }
 
-console.log(computerPlay());
+console.log(userChoice());
